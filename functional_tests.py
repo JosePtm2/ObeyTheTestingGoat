3fb63293dfab8218bdb4a20e89264958747ece6b
JosePtm2 <jose.ferreira.ptm2@gmail.com>
2019-03-17 13:04:10 +0000

    created first model

--- functional_tests.py
+++ functional_tests.py
@@ -12,6 +12,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
 
         # FooName has heard about a cool new online toDoApp.
@@ -37,10 +42,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy stuff from superMarket',
-                      [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy stuff from superMarket')
         # There is still a text box inviting her to add another item. She
         # enters 'Go to the shoe shop'
         inputbox = self.browser.find_element_by_id('id_new_item')
@@ -49,13 +51,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # The page updates again, and now shows both items on her list
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy stuff from superMarket',
-                      [row.text for row in rows])
-
-        self.assertIn('2: Go to the shoe shop', [row.text for row in rows])
-
+        self.check_for_row_in_list_table('1: Buy stuff from superMarket')
+        self.check_for_row_in_list_table('2: Go to the shoe shop')
         # She notices that the site has generated a unique URL for her,
         # there is some explanatory text to that effect
 
--- lists/tests.py
+++ lists/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from lists.models import Item
 
 
 class HomePageTest(TestCase):
@@ -11,3 +12,24 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data={'item_text': 'A new Item'})
         self.assertIn('A new Item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
+
+
+class ItemModelTest(TestCase):
+
+    def create_a_new_item(self, item_text):
+        item = Item()
+        item.text = item_text
+        item.save()
+
+    def test_saving_and_retrieving_items(self):
+        self.create_a_new_item('The first EVAH')
+        self.create_a_new_item('The second')
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first = saved_items[0]
+        second = saved_items[1]
+
+        self.assertEquals(first.text, 'The first EVAH')
+        self.assertEquals(second.text, 'The second')
