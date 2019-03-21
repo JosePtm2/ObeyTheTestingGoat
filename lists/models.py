b7e90db62c8be861fc320637dfc250f13d57f9ba
JosePtm2 <jose.ferreira.ptm2@gmail.com>
2019-03-21 09:07:24 +0000

    new URL + view for adding to existing lists. FT passes =D

--- lists/models.py
+++ lists/models.py
@@ -9,4 +9,4 @@ class List(models.Model):
 
 class Item(models.Model):
     text = models.TextField(default='')
-    list = models.ForeignKey(List, default=None, on_delete='CASCADE')
+    list = models.ForeignKey(List, default=None, on_delete='CASCADE', null=True)
